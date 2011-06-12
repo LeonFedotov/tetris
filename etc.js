944a5f3a600bf2b821b77fbaaca6dbd0892982fe
Leon Fedotov <duejohn@gmail.com>
2011-06-13 02:39:06 +0300

    merging etc (foreach safe)

--- etc.js
+++ etc.js
@@ -10,3 +10,26 @@ String.prototype.repeat = function(n){
     n = n || 1;
     return Array(n+1).join(this);
 };
+
+if (!Array.prototype.forEach)
+{
+  Array.prototype.forEach = function(fun /*, thisp */)
+  {
+    "use strict";
+
+    if (this === void 0 || this === null)
+      throw new TypeError();
+
+    var t = Object(this);
+    var len = t.length >>> 0;
+    if (typeof fun !== "function")
+      throw new TypeError();
+
+    var thisp = arguments[1];
+    for (var i = 0; i < len; i++)
+    {
+      if (i in t)
+        fun.call(thisp, t[i], i, t);
+    }
+  };
+}
